4596437b37bff5456ee83e10180cf71d347d7cf9
Guilherme Del Rio <delrio_guilherme@yahoo.com>
2017-10-04 22:20:34 -0300

    added templates to the project

--- Desmo_class/first_app/views.py
+++ Desmo_class/first_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(resquest):
-    return HttpResponse('<em>Hello World!</em>')
+def index(request):
+    my_dict = {'insert_me':"Hello i am from views.py"}
+    return render(request,'first_app/index.html',context=my_dict)
